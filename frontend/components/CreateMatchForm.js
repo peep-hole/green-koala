961053cbdece942fd508ce52a9c353ea127f4d95
ajablonsk1 <adrian.tomasz.jablonski@gmail.com>
2022-04-11 16:25:37 +0200

    Added CreateMatchForm

---
+++ frontend/components/CreateMatchForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View} from "react-native-web";
+
+const CreateMatchForm = () => {
+    return (
+        <View style={}>
+        </View>
+    );
+};
+
+export default CreateMatchForm;
