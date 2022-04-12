23b555cf2a3b444e2f08f869c2963e14a9d28f47
peep-hole <62802600+peep-hole@users.noreply.github.com>
2022-04-12 22:19:05 +0200

    branch publish

--- backend/src/main/java/pl/edu/agh/api/RefereeApplication.java
+++ backend/src/main/java/pl/edu/agh/api/RefereeApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RefereeApplication {
+	private final boolean branch_publish = true;
 
 	public static void main(String[] args) {
 		SpringApplication.run(RefereeApplication.class, args);
