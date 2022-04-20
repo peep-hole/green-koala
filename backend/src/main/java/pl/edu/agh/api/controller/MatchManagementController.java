2a0f6de1c77fd11ecc140f763771e8f0965e7fe7
peep-hole <62802600+peep-hole@users.noreply.github.com>
2022-04-20 20:24:25 +0200

    backend rest api

--- backend/src/main/java/pl/edu/agh/api/controller/MatchManagementController.java
+++ backend/src/main/java/pl/edu/agh/api/controller/MatchManagementController.java
@@ -16,39 +16,6 @@ public class MatchManagementController {
     @Autowired
     private MatchManagementService service;
 
-    @GetMapping("/uniqId")
-    public UUID getUniqId() {
-        return UUID.randomUUID(); // TODO: add logic here
-    }
-
-    @GetMapping("/id/{id}")
-    public MatchManagement getById(@PathVariable String id) {
-        return service.find(id);
-    }
-
-    @GetMapping("/referee/main/{id}")
-    public String getSide1Token(@PathVariable String id){
-        return "Main Token for " + id;
-    }
-
-    @GetMapping("/referee/side1/{id}")
-    public String getSide2Token(@PathVariable String id){
-        return "Side 1 Token for " + id;
-    }
-
-    @GetMapping("/referee/side2/{id}")
-    public String getMainToken(@PathVariable String id){
-        return "Side 2 Token for " + id;
-    }
-
-    @PostMapping
-    public MatchManagement create(@RequestBody MatchManagement management) {
-        return service.save(management);
-    }
-
-
-    // v2 version
-
     @PostMapping("/newMatch")
     public void addNewMatch() {
         // TODO:
