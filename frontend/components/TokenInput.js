701bee30dde30771d252b3b58e4dda661843c096
kwdrt <jakubkcichocki@gmail.com>
2022-04-21 15:29:00 +0200

    placeholder API communication

--- frontend/components/TokenInput.js
+++ frontend/components/TokenInput.js
@@ -2,21 +2,54 @@ import React, { useEffect, useState } from 'react';
 import { Input, NativeBaseProvider, Text, Center, Button, VStack } from 'native-base';
 import FormHeader from './util/FormHeader';
 import { FontAwesome } from '@expo/vector-icons';
-
+import Api from './util/Api';
 //example call for App.js - <TokenInput userType="Organizer" />
 
-const MainRefereeTokenInput = props => {
+const TokenInput = props => {
     const [userType, setUserType] = useState('');
     const [error, setError] = useState(false);
-    const [clickAction, setClickAction] = useState(() => () => {
-        setError(false);
-        console.log('Mock sent token to backend');
-        setError(true);
+    const [token, setToken] = useState('');
+
+    // example match data (needs to be consulted with backend team)
+    const [matchData, setMatchData] = useState({
+        fighter1Name: 'Fighter1',
+        fighter2Name: 'Fighter2',
+        time: 0,
+        events: [],
     });
 
-    //we can set things like routes on button click depending on the type later
-    //my idea - get match data from token here and pass it as a prop to the next screen, otherwise show error if i.e. token does not exist
+    // temporary example request handler - for referee
+    // needs an endpoint on the backend which will return match data fitting the token and fail if token is invalid
+    // we can make different endpoints for different user types
+    // needs a route to match/organizer main screen after getting the data
+    const [clickAction, setClickAction] = useState(() => token => {
+        setError(false);
+        console.log(token);
+        console.log('/match/' + token);
 
+        Api.get(
+            '/match/' + token // temporary request body
+        )
+            .then(res => {
+                console.log(res);
+                console.log('Mock sent token to backend');
+
+                // waiting for backend
+                // if(props.userType != "Organizer"){
+                //     setMatchData(res);
+                // }
+                // route to another screen here
+            })
+            .catch(e => {
+                setError(true);
+                console.log(e);
+                console.log(token);
+                console.log('Could not send mock token to back');
+            });
+    });
+
+    //we can set things like routes on button click, function called on token submit depending on the type later
+    //my idea - get match data from token here and pass it as a prop to the next screen, otherwise show error if i.e. token does not exist
     useEffect(() => {
         switch (props.userType) {
             case 'Main':
@@ -26,7 +59,7 @@ const MainRefereeTokenInput = props => {
                 setUserType('Sędzia Boczny');
                 break;
             case 'Organizer':
-                setUserType('Organizator');
+                setUserType('Organizer');
                 break;
             default:
                 setUserType("We don't have that one, sorry");
@@ -53,19 +86,26 @@ const MainRefereeTokenInput = props => {
                     {userType}
                 </Text>
                 <VStack width="90%">
-                    <Input marginTop="20px" placeholder="Podaj kod spotkania"></Input>
+                    <Input
+                        marginTop="20px"
+                        placeholder="Podaj kod spotkania"
+                        onChangeText={value => {
+                            setToken(value);
+                        }}
+                    ></Input>
                     <Button
                         marginTop="20px"
                         marginRight="50px"
                         marginLeft="50px"
                         bg="#059669"
-                        onPress={clickAction}
+                        onPress={() => clickAction(token)}
                     >
                         Dołącz
                     </Button>
                     <Text color="red.500">
                         {error &&
                             'Unable to join the match, please check the token and try again'}
+                        {token}
                     </Text>
                 </VStack>
             </Center>
@@ -73,4 +113,4 @@ const MainRefereeTokenInput = props => {
     );
 };
 
-export default MainRefereeTokenInput;
+export default TokenInput;
