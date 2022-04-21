067d1fe08af52542b8c8f9f154615b30763708f0
kwdrt <jakubkcichocki@gmail.com>
2022-04-21 12:34:31 +0200

    saving local changes

--- frontend/components/LoginPick.js
+++ frontend/components/LoginPick.js
@@ -1,25 +1,28 @@
-import { Button } from 'react-bootstrap';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { Button } from 'react-native';
+import React from 'react';
 const LoginPick = () => {
-    return(
+    return (
         <>
-            <Text style={{textAlign: "center"}}>Login Screen</Text>
-            <Text style={{textAlign: "center"}}>Dołącz jako</Text>
+            <Text>Login Screen</Text>
+            <Text>Dołącz jako</Text>
             <View style={rolePickContainer}>
-                <Button style={rolePickStyle}>Sędzia główny</Button>
-                <Button style={rolePickStyle}>Sędzia boczny</Button>
-                <Button style={rolePickStyle}>Organizator</Button>
+                <Button style={rolePickStyle} title="Sędzia główny" onPress={() => {}} />
+                <Button title="Sędzia boczny" onPress={() => {}} />
+                <Button title="Organizator" onPress={() => {}} />
             </View>
-            
-            
-            <Button style={{bottom: 0, margin: 20}}>Info</Button> 
+
+            <Button title="Info"></Button>
         </>
-        
-    )
-}
+    );
+};
 
-const rolePickContainer = {display: "flex", justifyContent: "spaceBetween", flexDirection: "column", flexWrap: "nowrap"}
-const rolePickStyle = {padding: 50, margin: 20}
+const rolePickContainer = {
+    display: 'flex',
+    flexDirection: 'column',
+    flexWrap: 'nowrap',
+};
 
-export default LoginPick
+const rolePickStyle = { padding: 50, margin: 20 };
+
+export default LoginPick;
