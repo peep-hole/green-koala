9e9ecdc5affa715d4089d8918ac77af00cc007d4
kwdrt <jakubkcichocki@gmail.com>
2022-04-21 13:45:29 +0200

    added arrow and App.js call example

--- frontend/components/TokenInput.js
+++ frontend/components/TokenInput.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { Input, NativeBaseProvider, Text, Center, Button, VStack } from 'native-base';
 import FormHeader from './util/FormHeader';
+import { FontAwesome } from '@expo/vector-icons';
+
+//example call for App.js - <TokenInput userType="Organizer" />
 
 const MainRefereeTokenInput = props => {
     const [userType, setUserType] = useState('');
@@ -25,14 +28,25 @@ const MainRefereeTokenInput = props => {
     return (
         <>
             <FormHeader name="Token"></FormHeader>
-            <Center marginTop="150px">
+            <Button
+                marginBottom="150px"
+                width="50"
+                marginLeft={30}
+                bg="#059669"
+                text={{
+                    color: 'white',
+                }}
+            >
+                <FontAwesome name="arrow-circle-left" size={30} color="white"></FontAwesome>
+            </Button>
+            <Center>
                 <Text fontSize="20">Dołączasz do spotkania jako</Text>
                 <Text fontWeight="bold" fontSize="30">
                     {userType}
                 </Text>
                 <VStack width="90%">
                     <Input marginTop="20px" placeholder="Podaj kod spotkania"></Input>
-                    <Button marginTop="20px" marginRight="50px" marginLeft="50px">
+                    <Button marginTop="20px" marginRight="50px" marginLeft="50px" bg="#059669">
                         Dołącz
                     </Button>
                 </VStack>
