23712f09d163ec6b939c23fa79a408e9f60d6ab6
kwdrt <jakubkcichocki@gmail.com>
2022-04-25 20:05:44 +0200

    display close to mockup, no timer yet

--- frontend/components/DisplayMatch.js
+++ frontend/components/DisplayMatch.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { HStack, VStack, Text, Button, Center } from 'native-base';
+import { HStack, VStack, Text, Button, Center, Box, Flex } from 'native-base';
 import FormHeader from './util/FormHeader';
 
 const match = {
@@ -25,27 +25,104 @@ const DisplayMatch = props => {
                     <Center>
                         <Text>{matchTime}</Text>
 
-                        <HStack>
-                            <Text>{fighter1Name}</Text>
-                            <Text>{fighter2Name}</Text>
+                        <HStack width="100%" mb="20px">
+                            <Box
+                                bg="red.400"
+                                p="5px"
+                                width="50%"
+                                borderColor="black"
+                                borderWidth="1"
+                                height="40px"
+                            >
+                                <Center>
+                                    <Text color="white">{fighter1Name}</Text>
+                                </Center>
+                            </Box>
+                            <Box
+                                bg="blue.400"
+                                p="5px"
+                                width="50%"
+                                borderColor="black"
+                                borderWidth="1"
+                                height="40px"
+                            >
+                                <Center>
+                                    <Text color="white">{fighter2Name}</Text>
+                                </Center>
+                            </Box>
                         </HStack>
 
-                        <HStack>
-                            <Text>{fighter1Score}</Text>
-                            <Text>{fighter2Score}</Text>
-                        </HStack>
+                        <Flex
+                            bg="gray.300"
+                            p="5%"
+                            width="auto"
+                            borderColor="black"
+                            borderWidth="1"
+                            rounded="lg"
+                            flexDirection="row"
+                            alignItems={'center'}
+                            mb="20px"
+                        >
+                            <Center
+                                bg="gray.300"
+                                _text={{
+                                    color: 'black',
+                                    fontWeight: 'bold',
+                                    fontSize: '4xl',
+                                }}
+                                width="120px"
+                                height="64px"
+                                borderColor="black"
+                                borderWidth="1"
+                            >
+                                {fighter1Score}
+                            </Center>
 
-                        <VStack>
-                            <Text>event1 placeholder</Text>
-                            <Text>event2 placeholder</Text>
-                        </VStack>
+                            <Center>
+                                <Text fontSize="md" p="20px">
+                                    Score
+                                </Text>
+                            </Center>
+
+                            <Center
+                                bg="gray.300"
+                                _text={{
+                                    color: 'black',
+                                    fontWeight: 'bold',
+                                    fontSize: '4xl',
+                                }}
+                                width="120px"
+                                height="64px"
+                                borderColor="black"
+                                borderWidth="1"
+                            >
+                                {fighter2Score}
+                            </Center>
+                        </Flex>
+
+                        <Box bg="gray.300" mb="20px" width="100%" height="200px">
+                            <VStack>
+                                <Center>
+                                    <Text>event1 placeholder</Text>
+                                </Center>
+                                <Center>
+                                    <Text>event2 placeholder</Text>
+                                </Center>
+                            </VStack>
+                        </Box>
 
                         <VStack>
                             <HStack>
-                                <Button>RED point</Button>
-                                <Button>BLUE point</Button>
+                                <Button width="50%" p="20px" bg="red.500">
+                                    RED point
+                                </Button>
+                                <Button width="50%" p="20px" bg="blue.500" n>
+                                    BLUE point
+                                </Button>
                             </HStack>
-                            <Button>No point</Button>
+                            <Button bg="gray.500" p="30px">
+                                No point
+                            </Button>
                         </VStack>
                     </Center>
                 </VStack>
