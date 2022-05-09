a288a4aaa26bef9a6de28340bd1dbf56c2189299
Tomasz Boroń <tomaszboron2k@gmail.com>
2022-05-09 23:26:09 +0200

    front: test build fail

--- frontend/App.js
+++ frontend/App.js
@@ -17,7 +17,7 @@ import DisplayMatch from "./components/DisplayMatch";
 
 export default function App() {
   return (
-      <NativeBaseProvider>
+      <NtiveBaseProvider>
           <SSRProvider>
               <NativeRouter>
                   <Routes>
