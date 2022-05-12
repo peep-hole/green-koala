0ddb695e33b25fc4549349edaf5968106e7ca740
KarolZajac <karol.zajac30@gmail.com>
2022-05-12 21:12:10 +0200

    [GK-121] Add organizator's view footer.

---
+++ frontend/components/util/AdminFooter.js
@@ -0,0 +1,64 @@
+import React, { useEffect } from 'react';
+import { Box, HStack, Icon, Pressable, Center, Text } from "native-base";
+import { Link } from "react-router-native"
+import { NativeBaseProvider } from 'native-base';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+import { Entypo } from '@expo/vector-icons';
+
+
+const AdminFooter = props => {
+    const [selected, setSelected] = React.useState(1);
+
+    useEffect(() => {
+        if (props) {
+            setSelected(props.selected)
+        }
+    }, []);
+
+    return (
+        <>
+            <Box flex={1} bg="white" safeAreaTop width="100%" alignSelf="center">
+                <Center flex={1}></Center>
+                <HStack bg="#065f46" alignItems="center" safeAreaBottom shadow={6}>
+
+                    <Pressable cursor="pointer" opacity={selected === 0 ? 1 : 0.5} py="3" flex={1} onPress={() => setSelected(0)}>
+                        <Link to="/fighterList">
+                            <Center>
+                                <Icon as={<Entypo name="users" size={40} color="black" />} color="white" size="lg" />
+                                <Text color="white" fontSize="16">
+                                    Fighters
+                                </Text>
+                            </Center>
+                        </Link>
+                    </Pressable>
+
+
+                    <Pressable cursor="pointer" opacity={selected === 1 ? 1 : 0.5} py="2" flex={1} onPress={() => setSelected(1)}>
+                        <Link to="/matchList">
+                            <Center>
+                                <Icon as={<MaterialIcons name="event-note" size={40} color="black" />} color="white" size="lg" />
+                                <Text color="white" fontSize="16">
+                                    Matches
+                                </Text>
+                            </Center>
+                        </Link>
+                    </Pressable>
+
+                    <Pressable cursor="pointer" opacity={selected === 2 ? 1 : 0.6} py="2" flex={1} onPress={() => setSelected(2)}>
+                        <Link to="/tournamentList">
+                            <Center>
+                                <Icon as={<MaterialIcons name="emoji-events" size={40} color="black" />} color="white" size="lg" />
+                                <Text color="white" fontSize="16">
+                                    Tournaments
+                                </Text>
+                            </Center>
+                        </Link>
+                    </Pressable>
+                </HStack>
+            </Box>
+        </>
+    );
+};
+
+export default AdminFooter;
