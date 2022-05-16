d6e966dbfd85286833c25184b53335583180d8e0
KarolZajac <karol.zajac30@gmail.com>
2022-05-16 18:27:46 +0200

    [GK-94] History component with match event list.

--- frontend/components/FightHistory.js
+++ frontend/components/FightHistory.js
@@ -1,14 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import FormHeader from './util/FormHeader';
 import MainRefereeFooter from './util/MainRefereeFooter';
 import DisplayScore from './DisplayScore';
-import { HStack, VStack, Text, Center, Box } from 'native-base';
+import { HStack, VStack, Text, Center, Box, FlatList, Spacer } from 'native-base';
 import { useLocation } from "react-router-native"
+import Api from './util/Api';
 
 
 const FightHistory = () => {
 
     const props = useLocation({});
+    const [events, setEvents] = useState([]);
+    const [eventsLoaded, setEventsLoaded] = useState(false);
+
+    const getMatchEvents = () => {
+        Api.get('/proper/endpoint/match/{id}' //TODO: special endpoint for getting match events
+        ).then(res => {
+            setEvents(res.data);
+            // console.log(res.data);
+            setEventsLoaded(true);
+        }).catch(e => {
+            console.log(e);
+        })
+    }
+
+    useEffect(() => {
+        getMatchEvents();
+        setEvents([
+            {
+                eventID: "1234324",
+                time: "00:12",
+                referee: "Side referee 1",
+                decision: "RED: attack, hit, 2 points IDK."
+            },
+            {
+                eventID: "1243543",
+                time: "01:00",
+                referee: "Side referee 2",
+                decision: "BLUE: defense, miss, 0 points IDK. RED: attack, hit, 1 points IDK."
+            },
+            {
+                eventID: "12342344",
+                time: "01:34",
+                referee: "Main referee",
+                decision: "RED: attack, hit, 2 points IDK."
+            },
+            {
+                eventID: "1213123324",
+                time: "02:33",
+                referee: "Main referee",
+                decision: "BLUE: defense, miss, 0 points IDK."
+            },
+        ])
+        // console.log(events);
+    }, [])
 
     return (
         <>
@@ -37,9 +82,32 @@ const FightHistory = () => {
                     </Center>
                 </VStack>
 
-                <Text marginTop={50} fontSize={25}>
-                    HISTORY COMPONENT HERE
-                </Text>
+                <Box id="oncoming_matches" height="auto" width="90%">
+                    <Text fontSize={20} marginBottom={3}>
+                        <Center>
+                            Event decisions
+                        </Center>
+                    </Text>
+                    {/* TODO: update filtering when model will contain bool determining if fight is over / or maybe score is not null? */}
+                    <FlatList backgroundColor="gray.200"
+                        data={events} renderItem={
+                            ({ item }) =>
+                                <Box borderBottomWidth={1} borderTopWidth={1} _dark={{ borderColor: "gray.800" }} borderColor="coolGray.400" pl="4" pr="5" py="2">
+                                    <HStack space={3} justifyContent="space-between">
+                                        <Text _dark={{ color: "warmGray.50" }} color="coolGray.800" bold>
+                                            {item.referee + ": "}
+                                        </Text>
+                                        <Spacer />
+                                        <Text fontSize="xs" _dark={{ color: "warmGray.50" }} color="coolGray.800" alignSelf="flex-start">
+                                            {item.time}
+                                        </Text>
+                                    </HStack>
+                                    <Text _dark={{ color: "warmGray.50" }} color="coolGray.800" alignSelf="flex-center">
+                                        {item.decision}
+                                    </Text>
+                                </Box>
+                        } keyExtractor={item => item.eventID} />
+                </Box>
 
             </Center >
 
