19de2cd27018432097b295773f00c6f00edc8cac
rprec <r.precikowski@gmail.com>
2022-05-23 22:50:10 +0200

    [GK-90] added functionality to start match

--- backend/src/main/java/pl/edu/agh/MatchStatusManager.java
+++ backend/src/main/java/pl/edu/agh/MatchStatusManager.java
@@ -1,18 +1,36 @@
 package pl.edu.agh;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.stereotype.Component;
+import pl.edu.agh.model.Match;
+import pl.edu.agh.service.MatchManagementService;
 import pl.edu.agh.websocket.DecisionMessage;
+
 import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 @Component
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 public class MatchStatusManager {
+    private final MatchManagementService matchManagementService;
+    Map<UUID, Match> matchMap = new HashMap<>();
+
     @PostConstruct
     public void init() {
 
     }
 
-    public void startMatch(){
-
+    public void startMatch(UUID matchId) {
+        Match match = matchManagementService.getMatchById(matchId);
+        matchMap.put(matchId, match);
     }
 
     public void endMatch() {
