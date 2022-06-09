bd7dbee89c06a7f498c58cc257737fa329519db7
rprec <r.precikowski@gmail.com>
2022-06-09 11:53:42 +0200

    [GK-105] cleanup

--- backend/src/main/java/pl/edu/agh/websocket/RefereeDecision.java
+++ backend/src/main/java/pl/edu/agh/websocket/RefereeDecision.java
@@ -17,15 +17,15 @@ public class RefereeDecision {
     private List<Event> decision;
     private UUID refereeToken;
 
-    public void reset(){
+    public void reset() {
         fighter1Points = null;
         fighter2Points = null;
         decision = null;
         refereeToken = null;
     }
 
-    public boolean isEmpty(){
-        if (fighter1Points ==null|| fighter2Points == null || decision==null)
+    public boolean isEmpty() {
+        if (fighter1Points == null || fighter2Points == null || decision == null)
             return true;
         if (fighter1Points == 0 && fighter2Points == 0 && decision.isEmpty())
             return true;
