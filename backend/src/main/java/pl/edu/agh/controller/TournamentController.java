9669f29a1b40fd9d06d198b2d42c766085cfa791
rprec <r.precikowski@gmail.com>
2022-06-10 14:05:34 +0200

    [GK-136] simple controller and service implementation

--- backend/src/main/java/pl/edu/agh/controller/TournamentController.java
+++ backend/src/main/java/pl/edu/agh/controller/TournamentController.java
@@ -1,4 +1,35 @@
 package pl.edu.agh.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pl.edu.agh.model.Tournament;
+import pl.edu.agh.service.TournamentService;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/tournaments")
 public class TournamentController {
+    private final TournamentService tournamentService;
+
+    @PostMapping("/new-tournament")
+    public ResponseEntity<Void> addNewTournament(@RequestBody Tournament tournament) {
+        tournamentService.add(tournament);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteNewTournament(@PathVariable String id) {
+        Long idLong = Long.valueOf(id);
+        tournamentService.remove(idLong);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/get-all")
+    public ResponseEntity<List<Tournament>> deleteNewTournament() {
+        return new ResponseEntity<>(tournamentService.getAll(), HttpStatus.OK);
+    }
 }
--- backend/src/main/java/pl/edu/agh/service/TournamentService.java
+++ backend/src/main/java/pl/edu/agh/service/TournamentService.java
@@ -2,7 +2,10 @@ package pl.edu.agh.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.edu.agh.model.Tournament;
 import pl.edu.agh.repository.TournamentRepository;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -11,4 +14,19 @@ public class TournamentService {
     private final TournamentRepository tournamentRepository;
 
 
+    public void add(Tournament tournament) {
+        tournamentRepository.save(tournament);
+    }
+
+    public void remove(Long id) {
+        tournamentRepository.deleteById(id);
+    }
+
+    public Tournament getById(Long id) {
+        return tournamentRepository.findById(id).orElse(null);
+    }
+
+    public List<Tournament> getAll() {
+        return tournamentRepository.findAll();
+    }
 }
